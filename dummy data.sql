970ddcce024550ebbc8dbce9fc8543d27131fceb
mariia-mentiu <mentu_maria@ukr.net>
2014-10-21 13:46:00 +0200

    Update dummy data.sql
    add 2 more dummies

--- dummy data.sql
+++ dummy data.sql
@@ -1,13 +1,31 @@
-INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) VALUES ('mail@mail','test','nick','male','bla','blub','countryX','12454','MyStreet','123d')
+INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) 
+VALUES ('mail@mail','test','nick','male','bla','blub','countryX','12454','MyStreet','123d')
 
 INSERT INTO PROFILE VALUES(1,'35','http://thumbs.dreamstime.com/z/young-good-looking-male-close-up-portrait-handsome-man-34645758.jpg','single','brown','blue','190','80','doctor','english','none','nothing','blablubb', 123.4,456.7);
 
 INSERT INTO INTERESTEDIN
 VALUES(1,'single','female','brown','blue','160','170','50','60','30','40','something','blablubb','none','none');
 
-INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) VALUES ('mail2@mail','test2','nick2','female','bla','blub','countryX','12454','AnotherStreet','123d')
+INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber)
+VALUES ('mail2@mail','test2','nick2','female','bla','blub','countryX','12454','AnotherStreet','123d')
 
 INSERT INTO PROFILE VALUES(2,'35','http://zoomgirls.net/wallpapers/demi_lovato-1680x1050.jpg','single','brown','blue','170','60','lawyer','english','none','nothing','blablubb', 123.4,456.7);
 
 INSERT INTO INTERESTEDIN
 VALUES(2,'single','male','brown','blue','180','190','70','90','30','40','something','blablubb','none','none');
+
+INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) 
+VALUES ('mail3@mail','test3','nick3','male','bla3','blub3','Germany','52074','Ahornstrasse','55')
+
+INSERT INTO PROFILE VALUES(1,'35','http://www.newsthinking.com/wp-content/uploads/2004/01/finally-a-guy-who.jpg','single','brown','green','180','75','engineer','english','none','nothing','blablubb', 50.778, 6.059);
+
+INSERT INTO INTERESTEDIN
+VALUES(1,'single','female','blond','blue','170','185','50','65','30','40','something','blablubb','none','none');
+
+INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) 
+VALUES ('mail4@mail','test4','nick4','female','bla4','blub4','Germany','52074','Kopernikusstrasse','14')
+
+INSERT INTO PROFILE VALUES(1,'25','http://www.worthytoshare.net/wp-content/uploads/2014/03/8876509-lily-pretty-girl.jpg','single','brown','brown','170','55','teacher','english','none','nothing','blablubb', 50.77976, 6.06418);
+
+INSERT INTO INTERESTEDIN
+VALUES(1,'single','male','brown','green','175','185','70','95','30','40','something','blablubb','none','none');
