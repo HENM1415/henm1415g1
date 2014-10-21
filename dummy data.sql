8e414684056d32fd90312e214f6f978baa1ef77c
mariia-mentiu <mentu_maria@ukr.net>
2014-10-21 13:49:07 +0200

    Update dummy data.sql
    fix to dummy data

--- dummy data.sql
+++ dummy data.sql
@@ -17,15 +17,15 @@ VALUES(2,'single','male','brown','blue','180','190','70','90','30','40','somethi
 INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) 
 VALUES ('mail3@mail','test3','nick3','male','bla3','blub3','Germany','52074','Ahornstrasse','55')
 
-INSERT INTO PROFILE VALUES(1,'35','http://www.newsthinking.com/wp-content/uploads/2004/01/finally-a-guy-who.jpg','single','brown','green','180','75','engineer','english','none','nothing','blablubb', 50.778, 6.059);
+INSERT INTO PROFILE VALUES(3,'35','http://www.newsthinking.com/wp-content/uploads/2004/01/finally-a-guy-who.jpg','single','brown','green','180','75','engineer','english','none','nothing','blablubb', 50.778, 6.059);
 
 INSERT INTO INTERESTEDIN
-VALUES(1,'single','female','blond','blue','170','185','50','65','30','40','something','blablubb','none','none');
+VALUES(3,'single','female','brown','brown','170','185','50','65','24','40','something','blablubb','none','none');
 
 INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) 
 VALUES ('mail4@mail','test4','nick4','female','bla4','blub4','Germany','52074','Kopernikusstrasse','14')
 
-INSERT INTO PROFILE VALUES(1,'25','http://www.worthytoshare.net/wp-content/uploads/2014/03/8876509-lily-pretty-girl.jpg','single','brown','brown','170','55','teacher','english','none','nothing','blablubb', 50.77976, 6.06418);
+INSERT INTO PROFILE VALUES(4,'25','http://www.worthytoshare.net/wp-content/uploads/2014/03/8876509-lily-pretty-girl.jpg','single','brown','brown','170','55','teacher','english','none','nothing','blablubb', 50.77976, 6.06418);
 
 INSERT INTO INTERESTEDIN
-VALUES(1,'single','male','brown','green','175','185','70','95','30','40','something','blablubb','none','none');
+VALUES(4,'single','male','brown','green','175','185','70','95','30','40','something','blablubb','none','none');
