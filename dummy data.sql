aa546ce7fa55054a09bef8c763ebd8301001459e
Peter de Lange <peter.de.lange@rwth-aachen.de>
2014-10-21 13:06:43 +0200

    added dummy data

---
+++ dummy data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USER(email, password, nickname, gender, firstName, lastName, country, zipCode, streetName, streetNumber) VALUES ('mail@mail','test','nick','male','bla','blub','countryX','12454','MyStreet','123d')
+
+INSERT INTO PROFILE VALUES(1,'35','http://thumbs.dreamstime.com/z/young-good-looking-male-close-up-portrait-handsome-man-34645758.jpg','single','brown','blue','190','80','doctor','english','none','nothing','blablubb', 123.4,456.7);
+
+INSERT INTO INTERESTEDIN
+VALUES(1,'single','female','brown','blue','160','170','50','60','30','40','something','blablubb','none','none');
