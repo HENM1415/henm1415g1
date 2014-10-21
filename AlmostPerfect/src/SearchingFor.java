765a607b4539ce490bf77b8b4c9278c98f48f985
Peter de Lange <peter.de.lange@rwth-aachen.de>
2014-10-21 09:54:24 +0200

    added getter and setter for searchingFor class
    forgot in first commit..sorry

--- AlmostPerfect/src/SearchingFor.java
+++ AlmostPerfect/src/SearchingFor.java
@@ -20,19 +20,119 @@ public class SearchingFor {
     private String nationality;
     private String religion;
     private String hobbies;
-    private String additionalInfo;
+    private String minAge;
+    private String maxAge;
     
-    	
-	hairColor VARCHAR(255),
-	eyeColor VARCHAR(255),
-	heightMin VARCHAR(255),
-	heightMax VARCHAR(255),
-	weightMin VARCHAR(255),
-	weightMax VARCHAR(255),
-	ageMin VARCHAR(255),
-	ageMax VARCHAR(255),
-	profession VARCHAR(255),
-	nationality VARCHAR(255),
-	religion VARCHAR(255),
-	hobbies VARCHAR(255),
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getMinHeight() {
+        return minHeight;
+    }
+
+    public void setMinHeight(String minHeight) {
+        this.minHeight = minHeight;
+    }
+
+    public String getMinWeight() {
+        return minWeight;
+    }
+
+    public void setMinWeight(String minWeight) {
+        this.minWeight = minWeight;
+    }
+
+    public String getMaxHeight() {
+        return maxHeight;
+    }
+
+    public void setMaxHeight(String maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public String getMaWeight() {
+        return maWeight;
+    }
+
+    public void setMaWeight(String maWeight) {
+        this.maWeight = maWeight;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(String minAge) {
+        this.minAge = minAge;
+    }
+
+    public String getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(String maxAge) {
+        this.maxAge = maxAge;
+    }
+    
 }
