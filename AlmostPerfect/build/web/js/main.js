f3be41bc3dfbeb5725230ecd67bf0c70542d8893
AlexRuppert <sumpkrautjunkie@googlemail.com>
2014-10-21 13:15:27 +0200

    register page with proper names

--- AlmostPerfect/build/web/js/main.js
+++ AlmostPerfect/build/web/js/main.js
@@ -7,10 +7,23 @@ function init() {
 }
 
 function initEvents() {
+    var APP_NAME = 'AlmostPerfect';
   $("#loginPanelButton").click(openLoginPanel);
   $("#loginPanelCloseButton").click(closeLoginPanel);
   $("#loginButton").click(function(){
-
+    //Send a request with the data from the Login panel.
+    $.ajax({
+        method: 'POST',
+        //dataType: 'json',
+        url: 'authentication',
+        data: {username: $('#nameField').val(), password: $('#passwordField').val()},
+        success: function(resp) {
+            console.log(JSON.parse(resp));
+        },
+        error: function(resp) {
+            console.log('error', resp);
+        }
+    });
     closeLoginPanel();
   });
 
@@ -28,3 +41,15 @@ function openLoginPanel(){
 function closeLoginPanel(){
   $("#loginPanel").hide();
 }
+
+// Get user position and then call the callback function passing it the lat and lng.
+function getUserPosition(callback) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var coords = {lat: position.coords.latitude, lng: position.coords.longitute};
+            callback(coords);
+        });
+    } else {
+        alert('Unfortunately, Your browser is outdated and we cannot detect Your position!');
+    }
+};
